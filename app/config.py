949ad7915655254d80d5bcb14fe7c46481e48670
Faheem Fayaz <fayazfaheem01@gmail.com>
2025-07-13 07:36:07 +0600

    Update config.py

--- app/config.py
+++ app/config.py
@@ -12,7 +12,7 @@ class Settings(BaseSettings):
     access_token_expire_minutes:int
 
     
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding='utf-8')
 
 settings = Settings()
 
